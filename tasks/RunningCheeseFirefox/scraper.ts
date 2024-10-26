d31290f322d5458957c1dba4f5e28a2845df2821
xrgzs <xiaoran@xrgzs.top>
2024-10-26 17:38:58 +0800

    feat: add RunningCheeseFirefox

---
+++ tasks/RunningCheeseFirefox/scraper.ts
@@ -0,0 +1,11 @@
+import { Ok, Result } from "ts-results";
+import { ScraperReturned } from "../../src/class";
+
+export default async function (): Promise<Result<ScraperReturned, string>> {
+  // YOUR CODE HERE
+
+  return new Ok({
+    version: "0.0.0",
+    downloadLink: "https://www.lanzouo.com/b0ufrtlc",
+  });
+}
